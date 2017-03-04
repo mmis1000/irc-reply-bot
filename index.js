9b64e5f2cd7eb895fcfa19cbe70e0c32446faaaa
mmis1000 <mmis1000@yahoo.com.tw>
2017-03-04 08:24:36 +0000

    code format

--- index.js
+++ index.js
@@ -14,27 +14,28 @@ if (process.env.NO_LOADER) {
   })
   
   process.stdin.resume();//so the program will not close instantly
+  
   process.nextTick(function() {
-  exitHandler = function exitHandler(options, err) {
-    if (options.cleanup) console.log('[Loader] cleaning up...');
-    if (err) {
-      console.log(err.stack);
+    exitHandler = function exitHandler(options, err) {
+      if (options.cleanup) console.log('[Loader] cleaning up...');
+      if (err) {
+        console.log(err.stack);
+      }
+      if (options.exit) {
+        loader.destroy();
+      };
     }
-    if (options.exit) {
-      loader.destroy();
-    };
-  }
-  
-  //do something when app is closing
-  process.on('exit', exitHandler.bind(null,{cleanup:true}));
-  
-  //catches ctrl+c event
-  process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-  
-  //catches signal
-  process.on('SIGTERM', exitHandler.bind(null, {exit:true}));
-  
-  //catches uncaught exceptions
-  process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+    
+    //do something when app is closing
+    process.on('exit', exitHandler.bind(null,{cleanup:true}));
+    
+    //catches ctrl+c event
+    process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+    
+    //catches signal
+    process.on('SIGTERM', exitHandler.bind(null, {exit:true}));
+    
+    //catches uncaught exceptions
+    process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
   });
 }
