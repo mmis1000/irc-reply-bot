cfa3779e5ecd0ef351134bc9dee8db3d567dc867
mmis1000 <mmis1000@yahoo.com.tw>
2017-03-04 08:19:37 +0000

    improve compatibility for s2i

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-var Loader = require('./loader')
+var Loader, loader, exitHandler;
 
-var loader = new Loader('./bot.js')
-loader.initBot();
-loader.setTimeout(180 * 1000)
-
-loader.on('exit', function () {
-  process.exit();
-})
-
-process.stdin.resume();//so the program will not close instantly
-process.nextTick(function() {
-function exitHandler(options, err) {
-  if (options.cleanup) console.log('[Loader] cleaning up...');
-  if (err) {
-    console.log(err.stack);
+if (process.env.NO_LOADER) {
+  require("./bot.js");
+} else {
+  Loader = require('./loader')
+  
+  loader = new Loader('./bot.js')
+  loader.initBot();
+  loader.setTimeout(180 * 1000)
+  
+  loader.on('exit', function () {
+    process.exit();
+  })
+  
+  process.stdin.resume();//so the program will not close instantly
+  process.nextTick(function() {
+  exitHandler = function exitHandler(options, err) {
+    if (options.cleanup) console.log('[Loader] cleaning up...');
+    if (err) {
+      console.log(err.stack);
+    }
+    if (options.exit) {
+      loader.destroy();
+    };
   }
-  if (options.exit) {
-    loader.destroy();
-  };
+  
+  //do something when app is closing
+  process.on('exit', exitHandler.bind(null,{cleanup:true}));
+  
+  //catches ctrl+c event
+  process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+  
+  //catches signal
+  process.on('SIGTERM', exitHandler.bind(null, {exit:true}));
+  
+  //catches uncaught exceptions
+  process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+  });
 }
-
-//do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
-
-//catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-
-//catches signal
-process.on('SIGTERM', exitHandler.bind(null, {exit:true}));
-
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
-});
